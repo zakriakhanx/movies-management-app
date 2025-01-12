ca2a14f1ba1f65557012964d7d1a1b7fb9ad5e13
Zakria <zakriakhanz2003@gmail.com>
2025-01-12 22:20:32 +0500

    Add Movie detail page, favorites page, and update styles; integrate ReactPlayer for movie details

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const SearchBar = () => {
   return (
     <div className='w-full bg-black h-[40vh] flex items-center justify-center relative'>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img className='w-full h-[40vh] object-cover opacity-50' src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/PK-en-20250106-TRIFECTA-perspective_ac4f9910-e162-4463-9f26-4f7743230f6b_large.jpg" alt="" />
 
         <input className='bg-black text-white border-[1px] border-slate-100 absolute w-96 h-10 rounded-md px-3 opacity-80' type="text" />
+        <Link href={`/favorites`} className='absolute'>
         <button className='bg-gray-300 border-black border-[2px] absolute w-40 h-10 rounded-md text-md bottom-14'>Search</button>
+        </Link>
 
     </div>
   )
--- app/movieDetails/[id]/page.js
+++ app/movieDetails/[id]/page.js
@@ -1,14 +1,71 @@
-import { use } from "react"
+'use client'
+import { use, useRef } from "react"
 import React from 'react'
+import { useMovies } from "@/app/contextAPI/MoviesContext";
+import Image from 'next/image'
+import Link from 'next/link'
+import ReactPlayer from 'react-player'
+import { useState } from "react";
 
+const Page = ({ params }) => {
 
-const Page = ({params}) => {    
   const param = use(params);
 
+  const movieData = useMovies();
+
+  const [isFavorite, setIsFavorite] = useState(false);
+  const [favorite, setFavorite] = useState([]);
+  const imageRef = useRef(null);
+
+  const handleToggle = () => {
+    setIsFavorite((prevState) => !prevState);
+    if(isFavorite){
+      console.log("Removed from favorites");}
+    console.log(imageRef.current.id);
+
+  };
+
   return (
-    <div>
-      <h1>hello {param.id}</h1>
-    </div>
+    <>
+      {movieData.map((movie) => {
+        if (movie.trackId == param.id) {
+          return (
+            <div key={movie.trackId} className='flex flex-col bg-gray-300 p-5 h-full min-h-screen'>
+
+              <div className="bg-black flex justify-center items-center w-auto h-[40vh]">
+                <ReactPlayer url={movie.previewUrl} width='100%' height='100%' controls={true} playing={false} />
+              </div>
+
+              <div className="flex justify-around px-5 py-10 h-full cursor-default">
+
+                <div className="w-1/2">
+                  <h1 className="text-4xl mb-5">{movie.trackName}</h1>
+                  <p>{movie.longDescription}</p>
+                </div>
+
+                <div className="w-1/5">
+
+                  <div className="mb-5 flex items-center" >
+                    <Image src={isFavorite ? "/favoriteFilled.svg" : "/favorite.svg"} id={movie.trackId} alt={movie.trackId} width={30} height={30} onClick={handleToggle} className="cursor-pointer" ref={imageRef} />
+                    <h1 className="mx-2 cursor-default">Favorite</h1>
+                  </div>
+
+                  <div className="cursor-default">
+                    <h1>Director: {movie.artistName}</h1>
+                    <h1>Genre: {movie.primaryGenreName}</h1>
+                    <h1>ReleaseDate: {movie.releaseDate.split("T")[0]}</h1>
+                    <h1>Price: ${movie.trackPrice}</h1>
+                  </div>
+                </div>
+
+              </div>
+
+            </div>
+          )
+        }
+      }
+      )}
+    </>
   )
 }
 
