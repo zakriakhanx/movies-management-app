d017c02938c65c449347814e7796a7ff1afeebbd
Zakria <zakriakhanz2003@gmail.com>
2025-01-14 18:40:11 +0500

    Implement search functionality in SearchBar component; add input handling and dropdown for matching movies

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -1,19 +1,64 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { useMovies } from '../contextAPI/MoviesContext'
 
 const SearchBar = () => {
+
+  const movieData = useMovies();
+
+  const [inputValue, setInputValue] = useState('')
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const matchingMovies = inputValue
+    ? movieData.filter((movie) =>
+      movie.trackName.toLowerCase().startsWith(inputValue.toLowerCase())
+    )
+    : [];
+
+  if (matchingMovies.length > 0) {
+    console.log("Movies found:", matchingMovies);
+  } else {
+    console.log("No movies found!");
+  }
+
   return (
     <div className='w-full bg-background h-[40vh] flex items-center justify-center relative'>
       {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img className='w-full h-[40vh] object-cover opacity-50' src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/PK-en-20250106-TRIFECTA-perspective_ac4f9910-e162-4463-9f26-4f7743230f6b_large.jpg" alt="" />
+      <img className='w-full h-[40vh] object-cover opacity-50 absolute' src="https://assets.nflxext.com/ffe/siteui/vlv3/154a9550-ce07-4e28-819c-63185dd849f8/web/PK-en-20250106-TRIFECTA-perspective_ac4f9910-e162-4463-9f26-4f7743230f6b_large.jpg" alt="" />
 
-      <div className='absolute flex flex-col items-center gap-2 '>
-        <input className='bg-background text-textPrimary border-[1px] border-border h-10 rounded-md px-3 opacity-80 w-52 sm:w-96' type="text" />
-        <button className='bg-primary active:bg-secondary border-border border-[1px] w-36 h-10 rounded-md text-md text-textPrimary'>Search</button>
+      <div className="relative flex flex-col items-center gap-1">
+        <input
+          className="bg-background text-textPrimary border-[1px] border-black h-10 rounded-md px-3 opacity-80 w-52 sm:w-96"
+          type="text"
+          value={inputValue}
+          placeholder='Search Here'
+          onChange={handleChange}
+        />
+
+        {/* Dropdown */}
+        {matchingMovies.length > 0 && (
+          <div className="absolute top-full mt-1 w-52 sm:w-96 bg-background border-[1px] border-border rounded-md shadow-lg z-10">
+            {matchingMovies.map((movie) => (
+              <Link key={movie.trackId} href={`/movieDetails/${movie.trackId}`}>
+                <div
+                  className="text-textPrimary h-10 px-3 flex items-center hover:bg-accent cursor-pointer"
+                >
+                  {movie.trackName}
+                </div>
+              </Link>
+            ))}
+          </div>
+        )}
       </div>
 
+
     </div>
+
   )
 }
 
