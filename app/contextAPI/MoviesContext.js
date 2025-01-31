38c94467ed1f36341e0eed7db11e1d11a281a9d5
Zakria Khan <zakriakhanz2003@gmail.com>
2025-02-01 01:43:24 +0500

    Update MoviesContext.js

--- app/contextAPI/MoviesContext.js
+++ app/contextAPI/MoviesContext.js
@@ -3,7 +3,7 @@ import { createContext, useContext, useState, useCallback, useEffect } from "rea
 import axios from "axios"
 
 // API endpoint for fetching movie data from iTunes
-const MOVIES_API_URL = '/api/itunes/search?term=movies'
+const MOVIES_API_URL = '/api/itunes/search?term=movie'
 
 // Create context for managing movie data globally
 const MoviesContext = createContext()
